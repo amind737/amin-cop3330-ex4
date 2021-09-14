58c700dc1dae5b9d37f34dddb24a80765c370672
Dharik Amin <74932573+amind737@users.noreply.github.com>
2021-09-13 21:25:10 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,28 @@
+  /*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Dharik Amin
+ */
+
+package org.example;
+import java.util.Scanner;
+public class App 
+{
+    public static void main(String[] args)
+    {
+        Scanner keyboardInput = new Scanner(System.in);
+
+        System.out.print("Enter a noun: ");
+        String noun = keyboardInput.nextLine();
+
+        System.out.print("Enter a verb : ");
+        String Verb = keyboardInput.nextLine();
+
+        System.out.print("Enter an adjective: ");
+        String Adjective = keyboardInput.nextLine();
+
+        System.out.print("Enter an adverb: ");
+        String Adverb = keyboardInput.nextLine();
+
+        System.out.println("Do you " +Verb +" your " + Adjective +" "+ noun +" "+ Adverb+"? That's hilarious!");
+    }
+}
